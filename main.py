80350ae31c17be60e8d46ba7bf6c187bab5f3a07
pooja <2024ucp1717@mnit.ac.in>
2026-08-05 18:40:18 +0530

    Added activity logging

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import sys
 
+
+from datetime import datetime
+import os
+
+LOG_FILE = "logs/activity.log"
+
+def log_activity(option):
+    with open(LOG_FILE, "a") as file:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{current_time} - {option}\n")
+
 def display_menu():
     """Displays the main CLI menu for CryptoLabX."""
     print("\n" + "=" * 40)
@@ -16,16 +27,20 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice (1-5): ").strip()
-
         if choice == '1':
+            log_activity("Encrypt")
             print("\n[+] Encrypt module: Coming Soon!")
         elif choice == '2':
+            log_activity("Decrypt")
             print("\n[+] Decrypt module: Coming Soon!")
         elif choice == '3':
+            log_activity("Attack")
             print("\n[+] Attack module: Coming Soon!")
         elif choice == '4':
+            log_activity("Analyze")
             print("\n[+] Analyze module: Coming Soon!")
         elif choice == '5':
+            log_activity("Exit")
             print("\nExiting CryptoLabX. Goodbye!")
             sys.exit(0)
         else:
