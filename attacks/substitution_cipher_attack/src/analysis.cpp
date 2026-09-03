d7c45ef8abe9e61b037b72b0594a548ce09618e3
pooja <2024ucp1717>
2026-09-03 12:16:08 +0530

    added word frequency analysis

--- attacks/substitution_cipher_attack/src/analysis.cpp
+++ attacks/substitution_cipher_attack/src/analysis.cpp
@@ -55,3 +55,164 @@ void frequency_analysis(string ciphertext){
     cout << "Common plaintext letters: E T A O I N\n";
 }
 
+void word_frequency_analysis(string ciphertext){
+    vector<string> words =extract_words(ciphertext);
+    map<string, int> word_count;
+    for (string word : words){
+        word_count[word]++;
+    }
+    cout << "        WORD FREQUENCY ANALYSIS\n";
+    cout << "\nONE-LETTER WORDS\n";
+    bool found = false;
+    for (auto item : word_count){
+        if (item.first.length() == 1)
+        {
+            cout << item.first
+                 << " -> "
+                 << item.second
+                 << endl;
+
+            found = true;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "None\n";
+    }
+
+    cout << "\nPossible plaintext letters: A or I\n";
+
+    cout << "\nTWO-LETTER WORDS\n";
+
+    found = false;
+
+    for (auto item : word_count)
+    {
+        if (item.first.length() == 2)
+        {
+            cout << item.first
+                 << " -> "
+                 << item.second
+                 << endl;
+
+            found = true;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "None\n";
+    }
+
+    cout << "\nTHREE-LETTER WORDS\n";
+    found = false;
+    for (auto item : word_count)
+    {
+        if (item.first.length() == 3)
+        {
+            cout << item.first
+                 << " -> "
+                 << item.second
+                 << endl;
+
+            found = true;
+        }
+    }
+    if (!found)
+    {
+        cout << "None\n";
+    }
+
+    cout << "\nREPEATED WORDS\n";
+
+    found = false;
+
+    for (auto item : word_count)
+    {
+        if (item.second > 1)
+        {
+            cout << item.first
+                 << " -> "
+                 << item.second
+                 << endl;
+
+            found = true;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "No repeated words found.\n";
+    }
+}
+
+string get_pattern(string word){
+    int mapping[26];
+    for (int i = 0; i < 26; i++)
+    {
+        mapping[i] = -1;
+    }
+    int next_number = 0;
+    string pattern = "";
+    for (int i = 0; i < word.length(); i++)
+    {
+        int index = word[i] - 'A';
+
+        if (mapping[index] == -1)
+        {
+            mapping[index] = next_number;
+            next_number++;
+        }
+        pattern += to_string(mapping[index]);
+
+        if (i != word.length() - 1)
+        {
+            pattern += "-";
+        }
+    }
+    return pattern;
+}
+
+void pattern_analysis(string ciphertext){
+    vector<string> words =
+        extract_words(ciphertext);
+    map<string, int> pattern_count;
+    map<string, vector<string>> pattern_words;
+    for (string word : words){
+        string pattern =
+            get_pattern(word);
+
+        pattern_count[pattern]++;
+        pattern_words[pattern].push_back(word);
+    }
+    cout << "           PATTERN ANALYSIS\n";
+    cout << "REPEATED PATTERNS\n";
+    bool found = false;
+    for (auto item : pattern_count){
+        if (item.second > 1)
+        {
+            cout << "\nPattern: "
+                 << item.first
+                 << endl;
+
+            cout << "Count: "
+                 << item.second
+                 << endl;
+
+            cout << "Words: ";
+            for (string word :
+                 pattern_words[item.first])
+            {
+                cout << word << " ";
+            }
+            cout << endl;
+
+            found = true;
+        }
+    }
+    if (!found)
+    {
+        cout << "No repeated patterns found.\n";
+    }
+}
